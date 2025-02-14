35abfa69f5e07b4fe8b1b6fae8868ee81d20fef1
ashely0594 <adom0594@gmail.com>
2025-02-13 22:23:54 -0500

    ninth commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
-
-
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="navbar fixed top-0 left-0 w-full z-50 bg-black bg-opacity-60 p-2 flex justify-between items-center shadow-md rounded-full"> 
-      
+    <div 
+      className={`navbar fixed top-0 left-0 w-full z-50 bg-black bg-opacity-60 p-2 flex justify-between items-center shadow-md rounded-full ${
+        showNavbar ? 'visible' : 'hidden' 
+      }`} 
+    > 
       <ul className="flex gap-6 text-white">
         <li>
           <Link to="/" className="hover:text-blue-400 cursor-pointer">Home</Link> 
